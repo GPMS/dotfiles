e5a7de0406ac32bd814218f7cefb8bbaf71a550d
GPMS <gab.portela@hotmail.com>
2019-11-03 17:43:44 -0300

    Dunst configurations

--- Scripts/battery_notifications.sh
+++ Scripts/battery_notifications.sh
@@ -2,17 +2,16 @@
 
 while :
 do
-    batteryLevel=$(acpi -b |cut -d ' ' -f2| grep -o '[0-9]*')
+    batteryLevel=$(acpi -b |cut -d ' ' -f4| grep -o '[0-9]*')
     isCharging=$(acpi -b | grep -c "Charging")
 
     if [[ $isCharging -eq 1 && $batteryLevel -eq 100 ]]
     then
-        dunstify "Charge full"
+        dunstify -t 3000 "Charge full"
+    elif [[ $isCharging -eq 0 && $batteryLevel -lt 30 ]]
+    then
+        dunstify -t 3000 "Battery low..."
     fi
 
-    if [[ $isCharging -eq 0 && $batteryLevel -lt 30 ]]
-    then
-        dunstify "Battery low..."
-    fi
     sleep 10
 done
