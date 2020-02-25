bda36c3ac1c5b316220dc460ab608a4935d5cc55
GPMS <gab.portela@hotmail.com>
2020-02-25 17:46:03 -0300

    Added insert-song Playlist

--- Music/playlists/add_to_playlist.sh
+++ Music/playlists/add_to_playlist.sh
@@ -6,6 +6,7 @@ find *ED* -name "*.flac" | sort -V > playlists/EDs.m3u
 find *OP* -name "*.flac" | sort -V > playlists/OPs.m3u
 find *OST* -name "*.flac" | sort -V > playlists/OSTs.m3u
 find *Instrumental* -name "*.flac" | sort -V > playlists/Instrumental.m3u
+find *挿入歌* -name "*.flac" | sort -V > playlists/挿入歌.m3u
 
 # Get all unique names
 all=$(echo -e `find . -name "*.flac" -printf '%P\n' | cut -d "-" -f 1 | sort | uniq`)
