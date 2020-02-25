792c60000783a729630bf04077455e97d3569b61
GPMS <gab.portela@hotmail.com>
2020-02-25 15:50:36 -0300

    Added script to automatically add all playlist for each different series

---
+++ Music/playlists/add_to_playlist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ..
+
+find *ED* -name "*.flac" | sort -V > playlists/EDs.m3u
+find *OP* -name "*.flac" | sort -V > playlists/OPs.m3u
+find *OST* -name "*.flac" | sort -V > playlists/OSTs.m3u
+find *Instrumental* -name "*.flac" | sort -V > playlists/Instrumental.m3u
+
+# Get all unique names
+all=$(echo -e `find . -name "*.flac" -printf '%P\n' | cut -d "-" -f 1 | sort | uniq`)
+IFS=', ' read -r -a series <<< $all
+
+# Add all files
+for item in "${series[@]}"; do
+    echo "looking for $item"
+    find $item* -name "*.flac" | sort -V > playlists/$item.m3u
+done
