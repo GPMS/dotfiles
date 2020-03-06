6ded54fdb26ba725fe6aa24ae3f2ea58064d8606
GPMS <gab.portela@hotmail.com>
2020-03-06 14:41:21 -0300

    Added new playlists

--- Music/playlists/update_playlist.sh
+++ Music/playlists/update_playlist.sh
@@ -17,3 +17,10 @@ for item in "${series[@]}"; do
     echo "looking for $item"
     find $item-* -name "*.flac" | sort -V > playlists/$item.m3u
 done
+
+cd playlists
+
+cat ./この素晴らしい世界に祝福を!.m3u ./この素晴らしい世界に祝福を!2.m3u > konosuba.m3u
+cat ./オーバーロード.m3u ./オーバーロードⅡ.m3u ./オーバーロードⅢ.m3u > overlord.m3u
+cat ./ソードアート・オンライン_1.m3u ./ソードアート・オンライン_2.m3u > sao.m3u
+cat ./異世界かるてっと.m3u ./異世界かるてっと2.m3u > isekaiquartet.m3u
