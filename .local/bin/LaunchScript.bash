8a17f2d245df58a56c5eb534033a5148b797f5bc
Gabriel Portela Macedo Souza <gportelams@gmail.com>
2023-01-10 21:16:28 -0300

    Allow launching Python scripts from my LaunchScript

--- .local/bin/LaunchScript.bash
+++ .local/bin/LaunchScript.bash
@@ -2,7 +2,12 @@
 
 pushd ~/.local/bin
 
-SCRIPT=$(fd --regex ".(ba)?sh$" | dmenu -i -c -l 5 -p "Script:")
+
+py_scripts=`fd --regex ".py$"`
+shell_scripts=`fd --regex ".(ba)?sh$"`
+all_scripts=`echo "$py_scripts $shell_scripts"`
+
+SCRIPT=$(echo $all_scripts | sed "s/ /\n/g" | dmenu -i -c -l 5 -p "Script:")
 
 if [ -z "$SCRIPT" ]; then
     echo "No script!"
